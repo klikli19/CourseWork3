035d28ac176dcbdbe239a4d01bea66caa5a79c3a
klikli19 <103356373+klikli19@users.noreply.github.com>
2022-12-06 19:34:05 +0300

    done: Question, QuestionService, JavaQuestionService, JavaQuestionController + package Exception

---
+++ src/main/java/com/skypro/examinerservice/controller/JavaQuestionController.java
@@ -0,0 +1,37 @@
+package com.skypro.examinerservice.controller;
+
+import com.skypro.examinerservice.model.Question;
+import com.skypro.examinerservice.service.QuestionService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.websocket.server.PathParam;
+import java.util.Collection;
+
+@RestController
+@RequestMapping("/java")
+public class JavaQuestionController {
+    private final QuestionService service;
+
+
+    public JavaQuestionController(QuestionService service) {
+        this.service = service;
+    }
+
+    @GetMapping("add")
+    public Question add(@PathParam("question") String question, @PathParam("answer") String answer) {
+        return service.add(question, answer);
+    }
+
+    @GetMapping("/remove")
+    public Question remove(@PathParam("question") String question, @PathParam("answer") String answer) {
+        Question tmp = new Question(question, answer);
+        return service.remove(tmp);
+    }
+
+    @GetMapping
+    public Collection<Question> getAll() {
+        return service.getAll();
+    }
+}
---
+++ src/main/java/com/skypro/examinerservice/service/JavaQuestionService.java
@@ -0,0 +1,47 @@
+package com.skypro.examinerservice.service;
+
+import com.skypro.examinerservice.exception.QuestionNotFoundException;
+import com.skypro.examinerservice.exception.QuestionOnListException;
+import com.skypro.examinerservice.model.Question;
+import org.springframework.stereotype.Service;
+
+import java.util.*;
+
+@Service
+public class JavaQuestionService implements QuestionService{
+    private final Set<Question> questions = new HashSet<>();
+    private Random random;
+
+    @Override
+    public Question add(String question, String answer) {
+        return add(new Question(question, answer));
+    }
+
+    @Override
+    public Question add(Question question) {
+        if (questions.contains(question)){
+            throw new QuestionOnListException("Такой вопрос есть в списке вопросов для экзамена");
+        }
+        questions.add(question);
+        return question;
+    }
+
+    @Override
+    public Question remove(Question question) {
+        if (!questions.contains(question)) {
+            throw new QuestionNotFoundException("Данного вопроса нет в списке вопросов для экзамена");
+        }
+        questions.remove(question);
+        return question;
+    }
+
+    @Override
+    public Collection<Question> getAll() {
+        return questions;
+    }
+
+    @Override
+    public Question getRandomQuestion() {
+        return new ArrayList<>(questions).get(random.nextInt(questions.size()));
+    }
+}
