db2ee248cdb47bb7251409ad2dbd506a6fe533d9
klikli19 <103356373+klikli19@users.noreply.github.com>
2022-12-06 14:09:10 +0300

    start

---
+++ src/main/java/com/skypro/examinerservice/ExaminerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.skypro.examinerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExaminerServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExaminerServiceApplication.class, args);
+    }
+
+}
