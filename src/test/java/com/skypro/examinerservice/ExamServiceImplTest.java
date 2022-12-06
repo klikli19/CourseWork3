56cf46522ef00030266f18709d925c17e292be06
klikli19 <103356373+klikli19@users.noreply.github.com>
2022-12-06 23:25:03 +0300

    done: ExamServiceImplTest

---
+++ src/test/java/com/skypro/examinerservice/ExamServiceImplTest.java
@@ -0,0 +1,51 @@
+package com.skypro.examinerservice;
+
+import com.skypro.examinerservice.model.Question;
+import com.skypro.examinerservice.service.ExaminerServiceImpl;
+import com.skypro.examinerservice.service.JavaQuestionService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class ExamServiceImplTest {
+    @Mock
+    JavaQuestionService javaQuestionService;
+
+    @InjectMocks
+    ExaminerServiceImpl examinerService;
+
+    private Collection<Question> questions;
+
+    @BeforeEach
+    public void setUp() {
+         questions = Set.of(
+                new Question("test", "answer"),
+                new Question("test2", "answer2"),
+                new Question("test3", "answer3"),
+                new Question("test4", "answer4")
+        );
+        when(javaQuestionService.getAll()).thenReturn(questions);
+        when(javaQuestionService.getRandomQuestion()).thenReturn(
+                new Question("test", "answer"),
+                new Question("test2", "answer2"));
+    }
+
+    @Test
+    public void shouldReturnGetQuestions() {
+        Assertions.assertEquals(examinerService.getQuestions(5), questions);
+    }
+
+
+}
